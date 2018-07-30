c9558db39544daccd13c1581044933accc0d50a3
eanorton <edwardnorton@icloud.com>
2018-07-30 17:36:44 -0400

    need to persist the playlist once it is created

--- src/components/ArtistFrame.js
+++ src/components/ArtistFrame.js
@@ -13,6 +13,7 @@ const ArtistFrame = (props) => {
       <h1 className="artist-rec-name" style={{fontFamily: 'Raleway, sans-serif', letterSpacing: "2px"}} onClick={()=>{props.handleClick(artistObj[0])}}> {artistName.slice(0,1)} </h1>
 
       {props.artist.map(t=>
+        <React.Fragment>
         <iframe
           src={`https://open.spotify.com/embed?uri=${t.uri}`}
           title={t.name}
@@ -24,6 +25,8 @@ const ArtistFrame = (props) => {
           allowtransparency="true"
           allow="encrypted-media">
         </iframe>
+        <Icon className="add-track" name="plus"/>
+      </React.Fragment>
       )}
     </div>
   )
--- src/components/Artists.js
+++ src/components/Artists.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import ArtistFrame from "./ArtistFrame";
-import { Image } from "semantic-ui-react";
+import { Image, Icon } from "semantic-ui-react";
 
 class Artists extends Component {
 
@@ -15,6 +15,7 @@ class Artists extends Component {
         <h1 className="artist-name" style={{fontFamily: 'Raleway, sans-serif', letterSpacing: "2px"}}>{searchedArtistName}</h1>
 
         {this.props.searchedArtistTopTracks.map(t=>
+          <React.Fragment>
           <iframe
           src={`https://open.spotify.com/embed?uri=${t.uri}`}
           key={t.id}
@@ -24,7 +25,11 @@ class Artists extends Component {
           height="100"
           frameBorder="0.25"
           allowtransparency="true"
-          allow="encrypted-media"></iframe>)}
+          allow="encrypted-media">
+        </iframe>
+        <Icon className="add-track" name="plus"/>
+        </React.Fragment>
+      )}
 
           { this.props.allArtists.map(artist=><ArtistFrame handleClick={this.props.handleClick} artist={artist} searchedArtist={searchedArtistName} />) }
 
